106601b9301e4c9ffc1e4792aa48cee9ab065ee6
Damien Fleminks <dfleminks@dfleminks-ltm3.internal.salesforce.com>
2019-04-19 17:22:24 +0100

    Creating test LWC

--- qr-scanner/README.md
+++ qr-scanner/README.md
@@ -1,8 +1,12 @@
 # SFDX App
 
+QR Scanner lightning App
+
 ## Dev, Build and Test
 
 ## Resources
+
+- Third party QR Scanner used: https://github.com/zxing-js/library
 
 ## Description of Files and Directories
 
--- qr-scanner/force-app/main/default/lwc/qrscanner/qrscanner.js
+++ qr-scanner/force-app/main/default/lwc/qrscanner/qrscanner.js
@@ -1,3 +1,84 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
+import { loadScript, loadStyle } from 'lightning/platformResourceLoader';
+import qrscan_js from '@salesforce/resourceUrl/QRscanner_js';
+import qrscan_css from '@salesforce/resourceUrl/QRScanner_css';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
-export default class Qrscanner extends LightningElement {}
+export default class Qrscanner extends LightningElement {
+
+    @track error;
+    @track successMessage = '';
+ 
+    renderedCallback() { // invoke the method when component rendered or loaded
+        Promise.all([
+            loadStyle(this, qrscan_css), // qrscanner CSS File
+            loadScript(this, qrscan_js),  // qrscanner js lib file
+        ])
+        .then(() => { 
+            this.error = undefined;
+            // Call back function if scripts loaded successfully
+            window.console.log('Successfully Loaded scripts');
+            qrscango();
+        })
+        .catch(error => {
+            this.error = error;
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    title: 'Error!!',
+                    message: error.message,
+                    variant: 'error',
+                }),
+            );
+        });
+
+        function qrscango(){
+            window.console.log('Loaded qrscango');
+            let selectedDeviceId;
+            const codeReader = new ZXing.BrowserQRCodeReader()
+            window.console.log('ZXing code reader initialized')
+            codeReader.getVideoInputDevices()
+                .then((videoInputDevices) => {
+                    const sourceSelect = this.template.getElementById('sourceSelect')
+                    selectedDeviceId = videoInputDevices[0].deviceId
+                    if (videoInputDevices.length >= 1) {
+                        videoInputDevices.forEach((element) => {
+                            const sourceOption = document.createElement('option')
+                            sourceOption.text = element.label
+                            sourceOption.value = element.deviceId
+                            sourceSelect.appendChild(sourceOption)
+                        })
+                        sourceSelect.onchange = () => {
+                            selectedDeviceId = sourceSelect.value;
+                        };
+                        const sourceSelectPanel = this.template.getElementById('sourceSelectPanel')
+                        sourceSelectPanel.style.display = 'block'
+                    }
+                    this.template.getElementById('startButton').addEventListener('click', () => {
+                        codeReader.decodeFromInputVideoDevice(selectedDeviceId, 'video').then((result) => {
+                            window.console.log(result)
+                            this.template.getElementById('result').textContent = result.text
+                        }).catch((err) => {
+                            window.console.error(err)
+                            this.template.getElementById('result').textContent = err
+                        })
+                        window.console.log(`Started continous decode from camera with id ${selectedDeviceId}`)
+                    })
+                    this.template.getElementById('resetButton').addEventListener('click', () => {
+                        codeReader.reset()
+                        window.console.log('Reset.')
+                    })
+                })
+                .catch((err) => {
+                    window.console.error(err)
+                })
+        }
+
+        
+        
+    }
+
+
+    
+
+
+}
