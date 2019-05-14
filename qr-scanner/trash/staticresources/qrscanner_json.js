687a73b57a454ba92ec9cb8a6a6cdfd41fb2b005
Damien Fleminks <damien@fleminks.com>
2019-05-14 12:17:59 +0100

    Adding changes before styling

---
+++ qr-scanner/trash/staticresources/qrscanner_json.js
@@ -0,0 +1,7 @@
+const prodcutData =[
+    {"Quantity" : 1, "Product2Id" : "01t9E000005lm1HQAQ"},
+    {"Quantity" : 1, "Product2Id" : "01t9E000005lm1HQAQ"},
+    {"Quantity" : 2, "Product2Id" : "01t9E000005lm1HQAQ"},
+    {"Quantity" : 3, "Product2Id" : "01t9E000005h4FGQAY"},
+    {"Quantity" : 5, "Product2Id" : "01t9E000005h4FGQAY"}
+];
