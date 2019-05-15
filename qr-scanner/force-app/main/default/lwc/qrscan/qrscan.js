346cda801dd3303dcf2e50ed60104ca9ac3a3677
Damien Fleminks <damien@fleminks.com>
2019-05-15 17:20:56 +0100

    Added loading gif and compelted demo

--- qr-scanner/force-app/main/default/lwc/qrscan/qrscan.js
+++ qr-scanner/force-app/main/default/lwc/qrscan/qrscan.js
@@ -3,6 +3,8 @@ import QRSCAN_HTML from '@salesforce/resourceUrl/qrscanner_html';
 import QRSCAN_HEADER from '@salesforce/resourceUrl/QRScanner_app_header';
 import QRSCAN_CONTENT1 from '@salesforce/resourceUrl/QRScanner_app_content_before';
 import QRSCAN_CONTENT2 from '@salesforce/resourceUrl/QRScanner_app_content_after';
+import QRSCAN_CAMERA from '@salesforce/resourceUrl/QRScanner_app_camera';
+import QRSCAN_LOADING from '@salesforce/resourceUrl/QRScanner_app_loading';
 import qrcontroller from '@salesforce/apex/QrScanController.createOrder';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
@@ -11,6 +13,9 @@ export default class Qrscan extends LightningElement {
     qrscan_head = QRSCAN_HEADER;
     qrscan_cont_before = QRSCAN_CONTENT1;
     qrscan_cont_after = QRSCAN_CONTENT2; 
+    qrscan_cam = QRSCAN_CAMERA;
+    qrscan_load = QRSCAN_LOADING;
+    @track showloading = false;
     @track contentBefore = true;
     @track areDetailsVisible = false;
     @track confirmSuccess = false;
@@ -22,6 +27,7 @@ export default class Qrscan extends LightningElement {
         window.addEventListener('message', event => {
             window.console.log(event.data);
             if(event.data.isTrue){
+                this.showloading = true;
                 window.console.log('QR code Message received');
                 window.console.log(event.data.myOrder);
                 window.console.log('executing createOrder()');
@@ -32,6 +38,7 @@ export default class Qrscan extends LightningElement {
                         window.console.log(result);
                         if(result === 'success'){
                             this.contentBefore = false;
+                            this.showloading = false;
                             this.areDetailsVisible = false;
                             const evt = new ShowToastEvent({
                                 title: 'QR Code scanner',
